4cb28e0ab4fa038aae8918ac6fc614b53103ea89
falegria86 <falegria@ittepic.edu.mx>
2024-01-16 16:29:33 -0700

    Testing

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -10,12 +11,14 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly publicPath: string;
+    private readonly routes: Router;
 
     constructor(options: Options) {
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
 
         this.port = port;
         this.publicPath = public_path;
+        this.routes = routes;
     }
 
     async start() {
@@ -26,11 +29,7 @@ export class Server {
         this.app.use(express.static(this.publicPath));
 
         //*Routes
-        this.app.get('/api/todos', (req, res) => {
-            res.json([
-                { id: 1, text: 'Buy milk', createdAt: new Date() }
-            ])
-        })
+        this.app.use(this.routes)
 
         this.app.listen(this.port, () => {
             console.log(`Server running on port: ${this.port}`)
